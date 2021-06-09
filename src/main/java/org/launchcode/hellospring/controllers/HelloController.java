06e1ca53632e60b91c17210856afc7b3c5a46737
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-06-09 17:07:38 -0500

    Finished Section controller annotations

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
 //    // Handles request at path / hello
@@ -14,28 +15,27 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
+    //lives at /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
+    // lives at hello/hello
     //Handles request of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name +"!";
     }
 
     //Handles requests of the form /hello/launchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
+    // hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm(){
         return "<html>" +
                 "<body>" +
